053af79b965c0385248ac94ba6e9582f81f9e76b
Blake Miller <luckyblake02@gmail.com>
2025-12-17 13:48:37 -0600

    Refactor graphCert function to accept parameters

--- certGrabber.py
+++ certGrabber.py
@@ -3,35 +3,37 @@ from cryptography import x509
 from cryptography.hazmat.primitives import hashes
 import re
 import warnings
+import time
 
-#Just for my own sake. I kept getting warnings on one of my personal certs. This can be optionally commented out.
+#Completely optional. I got tired of seeing deprecation errors.
 warnings.filterwarnings("ignore", category=UserWarning)
 
-def graphCert():
+def graphCert(store, reg):
 
     certList = []
     i = 0
 
-    #Enumerate "MY" cert store on windows using ssl library. The results are 3 part objects.
-    for cert_bytes, encoding_type, trust in ssl.enum_certificates("MY"):
-        #Use x509 library to read in cert bytes
+    #Reading in certs via ssl enum certs is 3 parts: Bytes, encoding, and trust.
+    for cert_bytes, encoding_type, trust in ssl.enum_certificates(store):
         cert = x509.load_der_x509_certificate(cert_bytes)
-        #Grab SHA1 fingerprint using hashes library
+        #Get the fingerprint bytes
         fingerprint_bytes = cert.fingerprint(hashes.SHA1())
-        #Join everything together, set to lowercase hex (x), then zero-pad (0) 2 characters (2) for each byte
+        #Convert to lowercase hex (x), zero-pad to two characters (02).
         fingerprint_hex = "".join(f"{b:02x}" for b in fingerprint_bytes)
         certList.extend([cert.subject, fingerprint_hex])
 
     while i < len(certList):
-        #If you are looking for a specific cert
-        if re.search("pattern", str(certList[i])):
+        #Regex search for string to locate the exact cert
+        if re.search(reg, str(certList[i])):
             thumbprint = certList[i + 1]
         i += 2
+    return thumbprint
 
-    #Format for cert-based graph authentication
-    graphProperties = {'AppID': "appid", 
-                       'TenantID': "tenantid", 
-                       'CertificateThumbprint': thumbprint}
-    return graphProperties
+#What to find
+wtf = input("Please enter a string that can be used to regex match the cert: ")
+print()
+certStore = input("Please enter the cert store to look in (MY, CA, ROOT): ")
+print()
 
-print(graphCert())
+print(f"The certificate thumbprint is: {graphCert(certStore, wtf)}")
+time.sleep(5)
