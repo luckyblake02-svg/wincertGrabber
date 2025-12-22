aea5e81d9459fa68e4d886a46ffb676405af4e75
Blake Miller <luckyblake02@gmail.com>
2025-12-22 10:36:21 -0600

    Refactor certGrabber.py for clarity and comments

--- certGrabber.py
+++ certGrabber.py
@@ -1,39 +1,33 @@
-import ssl
-from cryptography import x509
-from cryptography.hazmat.primitives import hashes
-import re
-import warnings
-import time
+import ssl #ssl library for certs
+from cryptography import x509 #x509 library for certs
+from cryptography.hazmat.primitives import hashes #hash library
+import re #regex library
+import warnings #warnings library for suppression
 
-#Completely optional. I got tired of seeing deprecation errors.
-warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=UserWarning) #this warning would not go away, so I suppressed it
 
 def graphCert(store, reg):
 
     certList = []
     i = 0
 
-    #Reading in certs via ssl enum certs is 3 parts: Bytes, encoding, and trust.
-    for cert_bytes, encoding_type, trust in ssl.enum_certificates(store):
-        cert = x509.load_der_x509_certificate(cert_bytes)
-        #Get the fingerprint bytes
-        fingerprint_bytes = cert.fingerprint(hashes.SHA1())
-        #Convert to lowercase hex (x), zero-pad to two characters (02).
-        fingerprint_hex = "".join(f"{b:02x}" for b in fingerprint_bytes)
-        certList.extend([cert.subject, fingerprint_hex])
+    for cert_bytes, encoding_type, trust in ssl.enum_certificates(store): #ssl enum certs reads in 3 values by default
+        cert = x509.load_der_x509_certificate(cert_bytes) #load bytes of cert
+        fingerprint_bytes = cert.fingerprint(hashes.SHA1()) #hash the bytes to get the fingerprint
+        fingerprint_hex = "".join(f"{b:02x}" for b in fingerprint_bytes) #convert fingerprint to hex
+        certList.extend([cert.subject, fingerprint_hex]) #append subject and fingerprint to array
 
     while i < len(certList):
-        #Regex search for string to locate the exact cert
-        if re.search(reg, str(certList[i])):
+        if re.search(reg, str(certList[i])): #if user-inputted string is a match, store this cert
             thumbprint = certList[i + 1]
         i += 2
     return thumbprint
 
-#What to find
-wtf = input("Please enter a string that can be used to regex match the cert: ")
+wtf = input("Please enter a string that can be used to regex match the cert: ") #what to find
 print()
 certStore = input("Please enter the cert store to look in (MY, CA, ROOT): ")
 print()
 
 print(f"The certificate thumbprint is: {graphCert(certStore, wtf)}")
-time.sleep(5)
+print()
+input("Press enter to exit...")
